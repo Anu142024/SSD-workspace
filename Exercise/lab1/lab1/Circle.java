20643013650c6c8c166c19685d70631ee33611f4
Anu142024 <A.Das3353@students.leedsbeckett.ac.uk>
2025-12-03 20:51:59 +0000

    lab1

--- Exercise/lab1/lab1/Circle.java
+++ Exercise/lab1/lab1/Circle.java
@@ -1,27 +1,46 @@
 package lab1;
 
+/**
+ * Circle class. Provides a concrete implementation based on the {@link Shape} class.
+ *
+ * @author mdixon
+ */
 public class Circle extends Shape {
 
-    private int radius;
+    private int radius;// The radius of the circle
 
+    /**
+     * Gets the radius of the circle.
+     *
+     * @return the radius
+     */
     public int getRadius() {
         return radius;
     }
 
+    /**
+     * Sets the radius of the circle.
+     *
+     * @param radius the radius to set
+     */
     public void setRadius(int radius) {
         this.radius = radius;
     }
 
+    @Override
     public int getArea() {
 
         return (int)(Math.PI * radius * radius);
     }
 
+    @Override
     public String toString() {
         return "Radius of circle is " + radius + ", " + super.toString();
     }
 
+    //Constructor
     Circle() {
+        // call super-class constructor with number of sides for this shape, i.e. 1
         super(1);
     }
 
--- Exercise/lab1/lab1/Ellipse.java
+++ Exercise/lab1/lab1/Ellipse.java
@@ -1,19 +1,29 @@
 package lab1;
 
+/**
+ * Ellipse class. Provides a concrete implementation of an ellipse based on the {@link Rectangle} class.
+ *
+ * @author mdixon
+ */
 public class Ellipse extends Rectangle {
 
+    // No additional attributes required
+
+    @Override
     public int getArea() {
 
         return (int)(Math.PI * (super.getArea()/4));
     }
 
+    @Override
     public String toString() {
 
         return "Ellipse based on a " + super.toString();
     }
 
+    //Constructor
     Ellipse() {
-
+        // need to use set sides, since inherited constructor will set the sides to '4'
         this.setSides(1);
 
     }
